0ae738f243011a219cded6c54453d1a89a3506de
Chris <chrisashby@gocardless.com>
2018-12-13 16:16:51 +0000

    Fixed csv encoding

--- train_csv.py
+++ train_csv.py
@@ -9,7 +9,7 @@ def arg_parser():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # Data and model checkpoints directories
-    parser.add_argument('--training_csv', type=str, default='data/csv/input.txt',
+    parser.add_argument('--training_csv', type=str, default='data/csv/input.csv',
                         help='path to training csv')
 
     return parser
@@ -18,7 +18,7 @@ def arg_parser():
 if __name__ == '__main__':
     args = arg_parser().parse_args()
 
-    with open(args.training_csv) as csv_file:
+    with open(args.training_csv, encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file)
         columns = zip_longest(*csv_reader)
 
